14e79a3dc79dca6c57d3a1230e0dbca1bd96133f
Patrik Masiar <patrik@kontentino.com>
2019-10-05 19:59:07 +0200

    chat room loader

--- src/components/ChatRoom/index.js
+++ src/components/ChatRoom/index.js
@@ -2,44 +2,55 @@ import React from 'react';
 import style from './style.module.css';
 import PropTypes from 'prop-types';
 import Message from '../Message';
+import Spinner from "react-spinkit";
 
-const ChatRoom = ({...props}) => (
-  <div className="card text-white bg-dark mt-3">
-    <div className="card-header">{props.title}</div>
-    <div className="card-body">
-      <div className={style.chatList}>
-        {props.messages.map(message => {
-          const isMine = message.senderId === props.currentUserId;
-          return (
-            <Message
-              isMine={isMine}
-              message={message.text}
-              key={message.id}
-            />
-          )
-        })}
+const ChatRoom = ({...props}) => {
+  if (props.title.length === 0) {
+    return (
+      <div className={style.loaderWrapper}>
+        <Spinner name="ball-triangle-path" color="#f0f0f0" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="card text-white bg-dark mt-3">
+      <div className="card-header">{props.title}</div>
+      <div className="card-body">
+        <div className={style.chatList}>
+          {props.messages.map(message => {
+            const isMine = message.senderId === props.currentUserId;
+            return (
+              <Message
+                isMine={isMine}
+                message={message.text}
+                key={message.id}
+              />
+            )
+          })}
+        </div>
+      </div>
+      <div className="card-footer">
+        <div className={style.sendWrapper}>
+          <input
+            placeholder="type your message..."
+            className="form-control"
+            onChange={props.onNewMessageChange}
+            value={props.newMessageValue}
+          />
+          <button
+            type="button"
+            className="btn btn-success"
+            style={{marginLeft: 10}}
+            onClick={props.onAddSubmit}
+          >
+            SUBMIT 
+          </button>
+        </div>
       </div>
     </div>
-    <div className="card-footer">
-      <div className={style.sendWrapper}>
-        <input
-          placeholder="type your message..."
-          className="form-control"
-          onChange={props.onNewMessageChange}
-          value={props.newMessageValue}
-        />
-        <button
-          type="button"
-          className="btn btn-success"
-          style={{marginLeft: 10}}
-          onClick={props.onAddSubmit}
-        >
-          SUBMIT 
-        </button>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 ChatRoom.propTypes = {
   messages: PropTypes.array.isRequired,
