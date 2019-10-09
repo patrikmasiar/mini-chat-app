c6e3fe57fb41d8624480c3170576e050c9ac3a59
Patrik Masiar <patrik@kontentino.com>
2019-10-09 11:15:23 +0200

    submit chat form with enter

--- src/components/AddToChatForm/index.js
+++ src/components/AddToChatForm/index.js
@@ -3,9 +3,10 @@
 // button component
 import React from 'react';
 import PropTypes from 'prop-types';
+import KeyboardListener from '../KeyboardListener';
 
 const AddToChatForm = ({...props}) => (
-  <>
+  <KeyboardListener onEnterPress={props.chatroom.length === 0 || props.nickname.length === 0 ? () => null : props.onSubmit}>
     <h4>{props.title}</h4>
     <div className="form-group" style={{width: 350}}>
       <label htmlFor="nickname">Nickname</label>
@@ -46,7 +47,7 @@ const AddToChatForm = ({...props}) => (
     >
       BACK
     </button>
-  </>
+  </KeyboardListener>
 );
 
 AddToChatForm.propTypes = {
